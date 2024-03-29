34ee2060f7e3f21cac5698a4709f6b524f92183f
yasamanseyfollahiasl <75227157+yasamanseyfollahiasl@users.noreply.github.com>
2024-03-29 16:25:33 +0330

    Enemy placed in map

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public Transform target;
+    public Animator animator;
     public float speed = 5f;
     Rigidbody rig;
+
+    bool isMoving = true; // Variable to track movement
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rig = GetComponent<Rigidbody>();
         // Finding the player GameObject using tag "Player"
         GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
-        
+
         // Checking if playerObject is found
         if (playerObject != null)
         {
@@ -30,8 +34,8 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        // Checking if target is not null before chasing
-        if (target != null)
+        // Checking if target is not null before chasing and character is allowed to move
+        if (target != null && isMoving)
         {
             Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
             rig.MovePosition(pos);
@@ -43,7 +47,22 @@ public class Enemy : MonoBehaviour
     {
         if (other.CompareTag("Bullet"))
         {
-            Destroy(gameObject);
+            animator.SetBool("Kill", true);
+            isMoving = false; // Stop moving after being hit by a bullet
+
+            // Start coroutine to make the enemy disappear after 30 seconds of not moving
+            StartCoroutine(DisappearAfterDelay(30f));
+        }
+    }
+
+    IEnumerator DisappearAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        
+        // Check if the enemy is still not moving
+        if (!isMoving)
+        {
+            gameObject.SetActive(false); // Deactivate the enemy GameObject
         }
     }
 }
